bd8391ae5f644f0c4ac2044aa53d928d60516ed0
irdiavxhi <irdiavxhi@gmail.com>
2016-12-18 22:37:32 +0100

    latest changes before going to spring controllers chapter 3

--- src/main/java/com/mvc/controller/HomeController.java
+++ src/main/java/com/mvc/controller/HomeController.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/home")
+	@RequestMapping("/go-home-with-response-body")
 	@ResponseBody
-	public String goHome(){
+	public String goHomeWithResponseBody(){
 		return "Welcome Home";
+	}
+	
+	@RequestMapping("/")
+	public String welcomeInitial(){
+		return "welcome";
+	}
+	
+	@RequestMapping("/home")
+	public String goHome(){
+		return "home";
 	}
 }
